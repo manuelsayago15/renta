298d3b866cab615aa06075e00ca2bb706ad57607
Manuel Sayago <manuelsayago15@gmail.com>
2020-03-17 20:22:08 -0300

    Adding methods in login view

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -75,39 +75,44 @@
         return {
             data: '',
             resp: '',
+            //backgroundImg: '../assets/images/bg-login.jpg',
+
+            //Login data
             identificacion: '27052222',
             password: '123',
             dig_verificador: 'K',
-            backgroundImg: '../assets/images/bg-login.jpg'
+            rut: '',
+            pasaporte: '',
+            errors: []
         }
       },
       methods:{
         onUpload() {
-          //let fd = new FormData();
-          //fd.append('file', this.file)
-        const formData = new FormData();
-        formData.append('identificacion', this.identificacion);
-        formData.append('password', this.password);
-        formData.append('dig_verificador', this.dig_verificador);
+        	//let fd = new FormData();
+        	//fd.append('file', this.file)
+	        const formData = new FormData();
+	        formData.append('identificacion', this.identificacion);
+	        formData.append('password', this.password);
+	        formData.append('dig_verificador', this.dig_verificador);
 
-          axios.post('http://10.156.160.21:8000/api/login/', formData, {
-            /*data: {
-                identificacion: this.identificacion,
-                password: this.password,
-                dig_verificador: this.dig_verificador
-            },
-            headers: {
-                 //'Access-Control-Allow-Origin':'http://10.156.160.21:8000/',
-                 'Content-Type': 'application/json'
-             }*/
+			axios.post('http://10.156.160.21:8000/api/login/', formData, {
+			/*data: {
+			    identificacion: this.identificacion,
+			    password: this.password,
+			    dig_verificador: this.dig_verificador
+			},
+			headers: {
+			     //'Access-Control-Allow-Origin':'http://10.156.160.21:8000/',
+			     'Content-Type': 'application/json'
+			 }*/
 
-          }
-            ).then(function(){
-              console.log('SUCCESS!!');
-            })
-            .catch(function(){
-              console.log('FAILURE!!');
-            });
+			}
+			).then(function(){
+			  console.log('SUCCESS!!');
+			})
+			.catch(function(){
+			  console.log('FAILURE!!');
+			});
         },
 
         test() {
@@ -138,7 +143,156 @@
 
 
         //Login Methods
+        iniciarSesion: function() {
+            var errors = [];
+            
+            if (this.validarCamposVacios()) {
+              if (this.rut != "") {
+                var rut =this.rut;
+                var dig_ver;
+                var rut_no_dig_ver=0;
+            
+                rut = rut.split(".").join("");
+                rut = rut.replace('-', '');
+    
+                if (rut.length == 7)
+                    rut_no_dig_ver = rut.substring(0, 6);
+                else if (rut.length == 8)
+                    rut_no_dig_ver = rut.substring(0, 7);
+                else if (rut.length == 9)
+                    rut_no_dig_ver = rut.substring(0, 8);
+    
+                dig_ver = rut.charAt(rut.length - 1);
+              }
+              else
+                {
+                 if (this.pasaporte!= "") {
+                     rut_no_dig_ver = this.pasaporte;
+                     dig_ver = 'p';
+                 }
+                }
 
+	        	//let fd = new FormData();
+	        	//fd.append('file', this.file)
+		        const formData = new FormData();
+		        formData.append('identificacion', rut_no_dig_ver);
+		        formData.append('password', this.password);
+		        formData.append('dig_verificador', this.dig_verificador);
+
+				axios.post('http://10.156.160.21:8000/api/login/', formData, {
+				/*data: {
+				    identificacion: this.identificacion,
+				    password: this.password,
+				    dig_verificador: this.dig_verificador
+				},
+				headers: {
+				     //'Access-Control-Allow-Origin':'http://10.156.160.21:8000/',
+				     'Content-Type': 'application/json'
+				 }*/
+
+				}
+				).then(function(){
+				  console.log('SUCCESS!!');
+				})
+				.catch(function(){
+				  console.log('FAILURE!!');
+				});
+           	
+                var dominio = 'http://10.156.160.21:8000/api/'; //direccion de la api para consumir
+               // $('#content').html('<div class="loading"><img src="assets/images/loader.gif"/><br/></div>');
+                $.ajax({
+                    url: dominio + 'login',
+                    dataType: 'json',
+                    type: 'POST',
+                    data: {
+                        identificacion: rut_no_dig_ver,
+                        password: $('#passwordInput').val(),
+                        dig_verificador: dig_ver
+                    },
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                    crossDomain: true,
+                    success: function(resp) {
+                       
+                        var arrDatos = [];
+                        var id = resp.user.id;
+                        var rut_logueado = resp.user.identificacion;
+                        var dv_logueado = resp.user.dig_verificador;
+                        var id_tipo_usuario=resp.user.id_tipo_usuario;
+                        var nombre_logueado=resp.user.nombres+" "+resp.user.apellidos;
+                        var id_empresa=resp.user.id_empresa;
+                        var id_oficina=resp.oficina[0].id;
+
+                        window.sessionStorage.setItem('token', resp.token);
+                        window.localStorage.setItem('token', JSON.stringify(resp.token));
+                        window.localStorage.setItem('id_oficina', JSON.stringify(id_oficina));
+                        window.localStorage.setItem('id_tipo_usuario', JSON.stringify(id_tipo_usuario));
+                        window.localStorage.setItem('nombre_logueado', JSON.stringify(nombre_logueado));
+                        window.localStorage.setItem('id_empresa', JSON.stringify(id_empresa));
+                        window.localStorage.setItem('rutLogueado', JSON.stringify(rut_logueado));
+                        window.localStorage.setItem('dvLogueado', JSON.stringify(dv_logueado));
+                        window.localStorage.setItem('oficina', JSON.stringify(resp.oficina));
+                        $('#content').fadeIn(1000000);
+                        if(id_tipo_usuario=='1'){
+                            window.location = './page-dashboard/gerente-sucursal.html';
+                        }
+                        if(id_tipo_usuario=='11'){
+                            window.location = './page-dashboard/gerente-zonal.html';
+                        }
+                        if(id_tipo_usuario=='2'){
+                            window.location = './page-dashboard/ejecutivo.html';
+                        }
+                        if(id_tipo_usuario=='3'){
+                            window.location = './page-dashboard/intermediario.html';
+                        }
+                        if(id_tipo_usuario=='4'){
+                            window.location = './page-admin/admin-liquidadores.html';
+                        }
+                       
+                    },
+                    error: function(resp) {
+                        var errors = [];
+                        errors.push('<li>Credenciales inválidas.</li>');
+                        $('#alert').show();
+                        $('#message').show();
+                        $('#errors').show();
+                        $('#alert').addClass("alert alert-danger alert-dismissible fade show");
+                        $('#message').html("Por favor, valide (los) siguiente(s) error(es):");
+                        $('#errors').html(errors);
+                        $('#rutInput').val("");
+                        $('#passportInput').val("");
+                        $('#passwordInput').val("");
+                       // $('#content').fadeToggle();
+                       removeSpinner($('body'))
+                    }
+                }); 
+                return false;
+            }
+        },
+
+        validarCamposVacios: function() {
+
+            if (((this.rut) || (this.pasaporte)) && (this.password)) {
+                return true;
+            }
+            this.errors = [];
+
+            if ((!this.rut) && (!this.pasaporte)) {
+                this.errors.push("El rut o pasaporte es obligatorio");
+            }
+            if (!this.password) {
+                this.errors.push("La contraseña es obligatoria");
+            }
+        },
+
+      },
+
+      mounted: {
+
+      },
+
+      watch: {
 
       },
 
